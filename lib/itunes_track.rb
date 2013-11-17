7843baca7e9b1803bb042a5804e15e332657103c
kyoendo <postagie@gmail.com>
2013-11-17 13:13:17 +0900

    Define ItunesTrack class

--- lib/itunes_track.rb
+++ lib/itunes_track.rb
@@ -1,5 +1,63 @@
-require "itunes_track/version"
+require 'appscript'
+require 'csv'
+require 'ostruct'
 
-module ItunesTrack
-  # Your code goes here...
+include Appscript
+
+class ItunesTrack < OpenStruct
+  VERSION = "0.0.1"
+
+  ATTRS = %i(name time artist album genre rating played_count year composer track_count track_number disc_count disc_number played_count lyrics)
+  class << self
+    def build(*attrs)
+      @attrs = attrs.empty? ? ATTRS : attrs
+      track_attrs = []
+      itunes_tracks.each do |track|
+        track_attrs << begin
+          @attrs.inject({}) do |h, attr|
+            val = track.send(attr).get rescue ''
+            if val.is_a?(String) && val.encoding.to_s!='UTF-8'
+              val = val.force_encoding('UTF-8')
+            end
+            h[attr.intern] = val
+            h
+          end
+        end
+      end
+      tracks.push *track_attrs.map { |attrs| new attrs }
+    end
+
+    def full_build
+      build(*track_properties)
+    end
+
+    def tracks
+      @tracks ||= []
+    end
+
+    def itunes
+      @itunes ||= app('iTunes').tracks
+    end
+
+    def itunes_tracks
+      @itunes_tracks ||= itunes.get
+    end
+
+    def size
+      itunes_tracks.size
+    end
+
+    def track_properties
+      itunes.properties
+    end
+
+    def to_csv(path, attrs=@attrs)
+      CSV.open(path, 'wb') do |csv|
+        csv << attrs
+        tracks.each { |t| csv << attrs.map{ |attr| t.send attr } }
+      end
+    end
+  end
 end
+
+ITunesTrack = ItunesTrack
--- lib/itunes_track/version.rb
+++
@@ -1,3 +0,0 @@
-module ItunesTrack
-  VERSION = "0.0.1"
-end
